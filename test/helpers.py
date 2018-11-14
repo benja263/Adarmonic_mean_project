bf184316301451bd56a01ee4f57caf91e4a66f12
benja263 <32192769+benja263@users.noreply.github.com>
2018-11-14 19:14:14 +0100

    cleaned tweets

--- test/helpers.py
+++ test/helpers.py
@@ -1,7 +1,8 @@
 import os.path
+import pickle
 
-def access_folder(folder_name = 'data'):
-    
+
+def access_folder(folder_name='data'):
     '''
     Returns the folder path desired by putting as argument the name of the folder that is
     contained in the project folder, otherwise need to specify address.
@@ -14,5 +15,18 @@ def access_folder(folder_name = 'data'):
         return prj_folder + '/' + folder_name + '/'
     else:
         return prj_folder + '/' + 'data' + '/'
-    
-    
+
+
+def save(name, object, folder_name='data'):
+    path = access_folder(folder_name)
+    name = path + name + '.pickle'
+    with open(name, 'wb') as handle:
+        pickle.dump(object, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load(name, folder_name='data'):
+    path = access_folder(folder_name)
+    name = path + name + '.pickle'
+    with open(name, 'rb') as fp:
+        file = pickle.load(fp)
+    return file
---
+++ test/pre_process_text.py
@@ -0,0 +1,57 @@
+import os
+from nltk.corpus import stopwords
+import string
+
+
+dir = os.path.dirname(__file__)
+DOWNLOADED_DICTIONARY_PATH = os.path.join(dir,'Test_Set_3802_Pairs.txt')
+
+downloaded_dictionary = {}
+f = open(DOWNLOADED_DICTIONARY_PATH, 'rb')
+for word in f:
+    word = word.decode('utf8')
+    word = word.split()
+    downloaded_dictionary[word[1]] = word[3]
+f.close()
+
+
+def clean(tweet):
+    """
+    Function that cleans the tweet using the functions above and some regular expressions
+    to reduce the noise
+
+    Arguments: tweet (the tweet)
+
+    """
+    #Separates the contractions and the punctuation
+    stop_words = set(stopwords.words('english'))
+    words = tweet.split(" ")
+    punctuations = string.punctuation
+    punctuations = punctuations.replace("-", "")
+    punctuations = punctuations.replace("'", "")
+    table = str.maketrans('', '', punctuations)
+    cleaned_tweet = ""
+    for word in words:
+        if "http" not in word and word not in stop_words and not word.startswith('#') and not word.startswith('@'):
+            lower_word = word.lower()
+            lower_word = lower_word.translate(table)
+            if lower_word.isalpha():
+                cleaned_tweet += lower_word + " "
+    return correct_spell(cleaned_tweet)
+
+
+def correct_spell(tweet):
+    """
+    Function that uses the three dictionaries that we described above and replace noisy words
+
+    Arguments: tweet (the tweet)
+
+    """
+
+    tweet = tweet.split()
+    for i in range(len(tweet)):
+        if tweet[i] in downloaded_dictionary.keys():
+            tweet[i] = downloaded_dictionary[tweet[i]]
+    tweet = ' '.join(tweet)
+    return tweet
+
