4816cc42dfbe3e36703601c4035b2d4dbcb3a076
benja263 <32192769+benja263@users.noreply.github.com>
2018-11-17 13:07:30 +0100

    trained LDA models

--- test/pre_process_text.py
+++ test/pre_process_text.py
@@ -2,8 +2,10 @@ import os
 from nltk.corpus import stopwords
 import string
 from nltk.stem import SnowballStemmer
-from nltk.stem.porter import *
+from nltk.stem.wordnet import WordNetLemmatizer
+import nltk
 
+nltk.download('wordnet')
 dir = os.path.dirname(__file__)
 DOWNLOADED_DICTIONARY_PATH = os.path.join(dir,'Test_Set_3802_Pairs.txt')
 
@@ -22,23 +24,30 @@ def clean(tweet):
     to reduce the noise
 
     Arguments: tweet (the tweet)
-
+s
     """
     #Separates the contractions and the punctuation
+
     stop_words = set(stopwords.words('english'))
+    stop_words.update(set(['and', 'i', 'a', 'and', 'so', 'arnt', 'this', 'when', 'it', 'many', 'Many', 'so', 'cant',
+                            'Yes', 'yes', 'No', 'no', 'these', 'these', "can't", "won't", 'get', 'you', 'like', 'him',
+                       'her', 'his', 'hers', 'amp', 'via', 'this', 'say', 'just', 'the', 'see', 'sit', 'use', 'one',
+                           'want', 'make', 'look', 'know', 'thing' , 'need', 'would']))
     words = tweet.split(" ")
     punctuations = string.punctuation
     punctuations = punctuations.replace("-", "")
     punctuations = punctuations.replace("'", "")
     table = str.maketrans('', '', punctuations)
     cleaned_tweet = ""
-    stemmer = SnowballStemmer('english')
+    lemmatizer = WordNetLemmatizer()
     for word in words:
-        if "http" not in word and word not in stop_words and not word.startswith('#') and not word.startswith('@'):
-            lower_word = word.lower()
-            lower_word = lower_word.translate(table)
-            if lower_word.isalpha():
-                cleaned_tweet += stemmer.stem(lower_word) + " "
+        lower_word = word.lower()
+        lower_word = lower_word.translate(table)
+        lower_word = lemmatizer.lemmatize(lower_word)
+        if "http" not in lower_word and lower_word not in stop_words and not lower_word.startswith('#') and not lower_word.startswith('@')\
+               and lower_word.isalpha() and len(lower_word) > 2:
+
+            cleaned_tweet += lower_word + " "
     return correct_spell(cleaned_tweet)
 
 
